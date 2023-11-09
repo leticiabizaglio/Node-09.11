cd4d871093b950d48901796dcaf9d9257db71bfa
leticiabizaglio <leticia.bizaglio@aluno.senai.br>
2023-11-09 16:54:43 -0300

    feat: test get

--- src/server.js
+++ src/server.js
@@ -9,8 +9,22 @@ const app = express(); // Cria o servidor
 app.use(express.json()); // Permite que o servidor interprete o formato JSON
 
 app.get("/", (req, res) => {
+    const nome = req.query.nome;
+    const qualquerNome = req.query.anime;
+    const page = req.query.page;
+    const limit = req.query.limit;
     // Rota inicial
-    res.status(200).json({ message: "Hello GET!"});
+    res.status(200).json({ message: nome, anime: qualquerNome, pagina: page, limite: limit});
+})
+
+app.get("/:nome/:sobrenome/:idade/:nacionalidade/:time", (req, res) => {
+    const nome = req.params.nome;
+    const sobrenome = req.params.sobrenome;
+    const idade = req.params.idade;
+    const nacionalidade = req.params.nacionalidade;
+    const time = req.params.time;
+    // Rota com inicial
+    res.status(200).json({message:`Oiie! Meu nome é ${nome} ${sobrenome}, tenho ${idade} anos, sou ${nacionalidade} e torço para o ${time}`});    
 })
 
 app.post("/", (req, res) => {
@@ -18,7 +32,7 @@ app.post("/", (req, res) => {
     res.status(200).json({ message: "Hello POST!"});
 })
 
-app.put("/", (req, res) => {
+app.put("/admin/users", (req, res) => {
     // Rota para PUT
     res.status(200).json({ message: "Hello PUT!"});
 })
@@ -32,3 +46,9 @@ app.listen(port, () => {
     // Inicia o servidor
     console.log(`⚡Server started on http://localhost:${port}`);
 })
+
+
+
+
+
+
