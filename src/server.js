84dffb0ee8ba21b82faaafc61cc2d1c3ee9bd8d8
leticiabizaglio <leticia.bizaglio@aluno.senai.br>
2023-11-09 15:09:58 -0300

    feat: server config

---
+++ src/server.js
@@ -0,0 +1,19 @@
+import express  from "express"; // Serve para escutar as requisições
+import { config } from "dotenv"; // Serve para ler o arquivo .env
+
+config(); // Lê o arquivo .env
+
+const port = process.env.PORT || 5000; // Define a porta que será usada
+
+const app = express(); // Cria o servidor
+app.use(express.json()); // Permite que o servidor interprete o formato JSON
+
+app.get("/", (req, res) => {
+    // Rota inicial
+    res.status(200).json({ message: "Hello World!"});
+})
+
+app.listen(port, () => {
+    // Inicia o servidor
+    console.log(`⚡Server started on http://localhost:${port}`);
+})
